f2e0061d83c371acca09449d1c1c01ac1bac2d68
Venkata Ravila <venkataravila@gmail.com>
2020-02-14 17:19:11 -0600

    Added Login functionality

--- src/main/java/project1/LandingPageServlet.java
+++ src/main/java/project1/LandingPageServlet.java
@@ -3,10 +3,24 @@ package project1;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class LandingPageServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
-	{
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world!");
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	
+			throws IOException{
+
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		
+		if(user != null) {
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Hello, " + user.getNickname());
+		} else {
+			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+		}
+		
 	}
 }
